6bf9cec384ed5d11bcb3b8180207a623e243c32e
Jesus Chong <chongjap@gmail.com>
2020-01-19 12:17:27 -0400

    Inclusion de backend para salvar configuraciones

--- index.php
+++ index.php
@@ -9,6 +9,89 @@ License: GPL2
 */
 
 
+    /*
+    * Función para añadir una página al menú de administrador de wordpress
+    */
+    function vsf_plugin_menu(){
+        //Añade una página de menú a wordpress
+        add_menu_page('Ajustes Plugin Validator Subscription Flow',                //Título de la página
+                        'Flow Integración',                             //Título del menú
+                        'administrator',                                    //Rol que puede acceder
+                        'vsf-settings',            //Id de la página de opciones
+                        'vsf_content_page_settings',       //Función que pinta la página de configuración del plugin
+                       // 'dashicons-chart-line'   //Icono del menú
+                       //plugin_dir_url( __FILE__ ) . 'images/logo.png',
+                       'dashicons-shield-alt',
+                       80
+                    );                          
+    }
+    add_action('admin_menu','vsf_plugin_menu');
+
+    /*
+    * Función que pinta la página de configuración del plugin
+    */
+    function vsf_content_page_settings(){
+        ?>
+            <div class="container-vsf">
+                <div class="wrap" style="width: 48%; display: inline-block;" >
+                    <div>
+                        <h2>Configuración de Subscripción</h2>
+                        <form method="POST" action="options.php">
+                            <?php 
+                                settings_fields('vsf-settings-group');
+                                do_settings_sections( 'vsf-settings-group' ); 
+                            ?>
+                            <br>
+                            <label style="width: 130px; display: inline-block;"  for="vsf-user">Tiempo de subscripción (días):&nbsp;</label>
+                            <input  type="number" 
+                                    name="vsf-time-subscription" 
+                                    id="vsf-time-subscription" 
+                                    value="<?php echo get_option('vsf-time-subscription'); ?>" />
+                            
+                            <br><br>
+                            
+                            <h3>Credenciales de integración con <a target="_blank" href="https://www.flow.cl/">Flow</a></h3>
+                            <label style="width: 130px; display: inline-block;" for="vsf-apikey">ApiKey:&nbsp;</label>
+                            <input  type="text" 
+                                    name="vsf-apikey" 
+                                    id="vsf-apikey" 
+                                    value="<?php echo get_option('vsf-apikey'); ?>" />
+                        
+                            <br><br>
+                            <label style="width: 130px; display: inline-block;" for="vsf-apisecret">ApiSecret:&nbsp;</label>
+                            <input  type="text" 
+                                    name="vsf-apisecret" 
+                                    id="vsf-apisecret" 
+                                    value="<?php echo get_option('vsf-apisecret'); ?>" />
+
+
+
+                         
+                            <?php submit_button(); ?>
+                        </form>
+                    </div>
+                    
+                </div>
+            </div>
+            
+        <?php
+    }
+
+     /*
+    * Función que registra las opciones del formulario en una lista blanca para que puedan ser guardadas
+    */
+    function vsf_save_settings(){
+        register_setting('vsf-settings-group',
+                        'vsf-time-subscription',
+                        'number');
+        register_setting('vsf-settings-group',
+                        'vsf-apikey',
+                        'text');
+        register_setting('vsf-settings-group',
+                        'vsf-apisecret',
+                        'text');
+    }
+    add_action('admin_init','vsf_save_settings');
 
 function vsf_send( $apiKey,$secretKey, $params) {
     $url = 'https://www.flow.cl/api/payment/getStatusByFlowOrder';
@@ -97,11 +180,12 @@ if (!function_exists('base_url')) {
             //si es un subscriptor que intenta ver las paginas y tiene su pago vencido, se redirige al perfil para que
             // actualice su numero de orden de pago
             if($in_fronted && $is_subscriber){
+                    $page_redirect_by_invalid_pay_subscription='/websitedomesa/wp-admin/profile.php';
                     // en primer lugar se revisa la session
                     unset($_SESSION['granted_access']);
                     unset($_SESSION['granted_access_reason']);
                     if(isset( $_SESSION['granted_access']) && !$_SESSION['granted_access']){
-                        $page_redirect_by_invalid_pay_subscription='http://localhost/websitedomesa/wp-admin/profile.php';
+                        
                         header('Location: '.$page_redirect_by_invalid_pay_subscription);
                         die();
                     }else if(!isset( $_SESSION['granted_access'])){
@@ -116,19 +200,14 @@ if (!function_exists('base_url')) {
                             $_SESSION['granted_access_reason']=$result_validate['granted_access_reason'];
                             $_SESSION['granted_access']= $granted_access=$result_validate['granted_access'];
                             //$response_valid_pay_in_flow['payer']/"leacido@hotmail.com"
-
-
                             
                         }else{
                             $_SESSION['granted_access_reason']='Numero de orden de pago faltante.';
                             $_SESSION['granted_access']= $granted_access=false;
                         }   
                         
-                      
-
                         if(!$granted_access){
                             //si es un subscriptor con el pago vencido, no dejarlo ver ninguna pagina
-                            $page_redirect_by_invalid_pay_subscription='http://localhost/websitedomesa/wp-admin/profile.php';
                             header('Location: '.$page_redirect_by_invalid_pay_subscription);
                             die();
                         }
@@ -142,12 +221,11 @@ if (!function_exists('base_url')) {
             }
             
 
-            
-
+            /*
             ?>
-
             <script> console.log(<?php echo json_encode($response_valid_pay_in_flow)?>);</script>
             <?php
+            */
         } 
     
         // validate_payment_subscription($number_order_pay_subscription_saved_profile,wp_get_current_user()->data->user_email)
@@ -156,7 +234,12 @@ if (!function_exists('base_url')) {
         $granted_access=true;
         $granted_access_reason=true;
 
-        $response_valid_pay_in_flow=vsf_send( "2AFC320E-C7D5-4EF4-9EE2-81050C0CL3D4","e0b07350f768cc64cdad997d81e13e4e2e313a7e", array(
+        $apiKey=get_option('vsf-apikey');
+        $secretKey=get_option('vsf-apisecret');
+
+        //$response_valid_pay_in_flow=vsf_send( "2AFC320E-C7D5-4EF4-9EE2-81050C0CL3D4","e0b07350f768cc64cdad997d81e13e4e2e313a7e", array(
+        $response_valid_pay_in_flow=vsf_send( $apiKey,$secretKey,
+         array(    
             "flowOrder"=>$number_order_pay
         ));
 
@@ -165,14 +248,19 @@ if (!function_exists('base_url')) {
                 $granted_access_reason='Orden de pago invalida.';
                 $granted_access=false;
             }else{
-                $date_limit_subscription=explode(" ",$response_valid_pay_in_flow['paymentData']['date'])[0];
-                $date_limit_subscription = new DateTime($date_limit_subscription);
-                date_add($date_limit_subscription, date_interval_create_from_date_string('60 days'));
-                $date_current = new DateTime(date_format(new DateTime(), 'Y-m-d'));
 
-                if ($date_limit_subscription < $date_current) {
-                   $granted_access_reason='Orden de pago actual expirada.';
-                   $granted_access=false;
+                $timeSubscription=get_option('vsf-time-subscription');
+                if(!empty($timeSubscription) && is_numeric($timeSubscription)){
+                    $date_limit_subscription=explode(" ",$response_valid_pay_in_flow['paymentData']['date'])[0];
+                    $date_limit_subscription = new DateTime($date_limit_subscription);
+    
+                    date_add($date_limit_subscription, date_interval_create_from_date_string('60 days'));
+                    $date_current = new DateTime(date_format(new DateTime(), 'Y-m-d'));
+    
+                    if ($date_limit_subscription < $date_current) {
+                       $granted_access_reason='Orden de pago actual expirada.';
+                       $granted_access=false;
+                    }
                 }
             }
         }else{
@@ -242,7 +330,6 @@ if (!function_exists('base_url')) {
                         <br><br>
                         <a class="button button-primary" href='https://www.flow.cl/btn.php?token=5f3a8rj' target='_self'>
                             Pagar subscripción
-                            <!-- <img src='https://www.flow.cl/img/botones/btn-pagar-celeste.png'> -->
                         </a>
 
                     </label>
@@ -260,11 +347,5 @@ if (!function_exists('base_url')) {
 
 
  
-
-    // function isJson($string) {
-    //     json_decode($string);
-    //     return (json_last_error() == JSON_ERROR_NONE);
-    //    }
-
 
 ?>
